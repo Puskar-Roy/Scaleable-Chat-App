f6d1d5350b18409f293c3fdec78ad612c5d7578f
Puskar-Roy <puskarroy600@gmail.com>
2024-05-12 12:47:00 +0530

    feat : next-auth options

---
+++ lib/auth.ts
@@ -0,0 +1,42 @@
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { db } from "./db";
+export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+      if (!dbUser) {
+        token.id = user!.id;
+        return token;
+      }
+      return {
+        id: dbUser.id,
+        name: dbUser.email,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+    async session({token , session}) {
+        if(token){
+            session.user.id = token.id
+            session.user.name = token.name
+            session.user.email = token.email
+            session.user.image = token.picture
+        }
+    },
+  },
+};
--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,7 @@
 import { Redis } from "@upstash/redis";
+import { config } from "@/config/config";
 
 export const db = new Redis({
-  url: "dsdsbdfbdf",
-  token: "bdbdb",
+  url: config.REDIS_URL,
+  token: config.REDIS_TOKEN,
 });
