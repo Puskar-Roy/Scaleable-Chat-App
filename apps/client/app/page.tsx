1354df0be2bd0ecf76ed2e016aa365ca0e93d0f7
Puskar-Roy <puskarroy600@gmail.com>
2024-05-15 00:24:12 +0530

    feat : chat ui

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSocket } from "../context/SocketProvider";
+import { Button } from "../components/button";
+import { ScrollArea } from "../components/ui/scroll-area";
 
 export default function page() {
   const { sendMessage, messages } = useSocket();
@@ -10,22 +12,43 @@ export default function page() {
     sendMessage(message);
     setmessage("");
   };
+
+  const chatRef = useRef(null);
+
+  // Scroll to the bottom of the chat when component mounts or when messages change
+  useEffect(() => {
+    const chatElement = chatRef.current as unknown as HTMLDivElement;
+    chatElement.scrollTop = chatElement.scrollHeight;
+  }, [messages]);
   return (
-    <div className="w-[80%] h-screen mx-auto flex justify-center items-center flex-col">
-      <div>
-        <h1>Message Application</h1>
-        {messages.map((data) => (
-          <li className="">{data}</li>
-        ))}
+    <div className="w-[80%] h-screen mx-auto flex justify-center items-center flex-col gap-10">
+      <div className="w-full flex- justify-center items-center flex-col">
+        <h1 className="font-semibold font- text-2xl text-center mb-10">
+          Group Messages
+        </h1>
+
+        <div ref={chatRef} className="w-[90%] sm:w-[50%] mx-auto overflow-y-auto h-96 bg-gray-100 p-4">
+          {messages.reverse().map((data) => (
+            <li className="list-none font-medium text-base">{data}</li>
+          ))}
+        </div>
       </div>
-      <div className="">
+      <div className="flex gap-2">
         <input
           type="text"
           placeholder="Send a Message!"
+          className="rounded-lg bg-slate-200 focus:ring-1 focus:ring-slate-950 text-slate-800 font-semibold"
           value={message}
           onChange={(e) => setmessage(e.target.value)}
         />
-        <button onClick={() => handleClick({ message })}>Send</button>
+
+        {/* <button
+          className="px-3 py-2 bg-slate-800 text-white rounded-lg hover:bg-slate-700"
+          onClick={() => handleClick({ message })}
+        >
+          Send
+        </button> */}
+        <Button onClick={() => handleClick({ message })}> Send</Button>
       </div>
     </div>
   );
